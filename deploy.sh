c57e98e44fdbd12c19fa25b009db6a3684049bea
bansalakshit <akshit@Akshits-MacBook-Pro.local>
2025-12-02 14:37:07 +0530

    modify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo "🚀 Starting Meraab Admin Production Deployment"
+echo "🚀 Starting Meraab Admin (Strapi) Production Deployment"
+
 
 # Go to your project folder
 cd /home/ubuntu/Meraab-Backend/Meraabadmin || exit
@@ -7,7 +8,7 @@ cd /home/ubuntu/Meraab-Backend/Meraabadmin || exit
 echo "📦 Installing production dependencies..."
 npm install --only=prod
 
-echo "🛠 Building Strapi Admin with extended memory..."
+echo "🛠 Building Strapi Admin UI..."
 export NODE_OPTIONS="--max-old-space-size=3072"
 npm run build
 
@@ -22,11 +23,6 @@ pm2 save
 
 echo "🔧 Enabling PM2 auto-start on reboot..."
 pm2 startup systemd -u ubuntu --hp /home/ubuntu
-
-echo "📜 Setting up log rotation..."
-pm2 install pm2-logrotate
-pm2 set pm2-logrotate:max_size 10M
-pm2 set pm2-logrotate:retain 5
 
 echo "🎉 Deployment Complete!"
 echo "Meraab Admin is now running in production."
